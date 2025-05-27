08b42569a5a63c801f2640c15a1e1df2862543cb
Fercan Şen <fercan.sen@p.tech>
2025-05-28 00:51:47 +0300

    Add weather card to display response

---
+++ src/components/WeatherCard.tsx
@@ -0,0 +1,50 @@
+import { WeatherData } from "@/types/WeatherData";
+import Icon from "./Icon";
+import { IconSize } from "@/enums/IconSize";
+import { Droplet, Thermometer, Wind } from "lucide-react";
+import { getWeatherIcon } from "@/utils/getWeatherIcon";
+
+interface WeatherCardProps {
+  data: WeatherData;
+  isMetric: boolean;
+}
+
+export default function WeatherCard({ data, isMetric }: WeatherCardProps) {
+  const unitSymbol = isMetric ? "C" : "F";
+  const WeatherIcon = getWeatherIcon(data.weather[0].description);
+  return (
+    <div className="bg-white text-black shadow-xl rounded-lg w-1/4  p-4 space-y-3">
+      <h2 className="text-xl font-semibold">
+        {data.name}, {data.sys?.country}
+      </h2>
+
+      <hr className="border-t border-gray-300" />
+
+      <div className="flex items-center gap-2">
+        <Icon icon={WeatherIcon} size={IconSize.LG} />
+        <span className="capitalize">{data.weather[0].description}</span>
+      </div>
+
+      <hr className="border-t border-gray-300" />
+
+      <div className="space-y-2">
+        <div className="flex items-center gap-2">
+          <Icon icon={Thermometer} size={IconSize.LG} />
+          <span>
+            Temp: {data.main.temp}°{unitSymbol}
+          </span>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Icon icon={Droplet} size={IconSize.LG} />
+          <span>Humidity: {data.main.humidity}%</span>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Icon icon={Wind} size={IconSize.LG} />
+          <span>Wind: {data.wind.speed} m/s</span>
+        </div>
+      </div>
+    </div>
+  );
+}
